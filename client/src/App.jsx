cd4cfb4413150c465f0e7f8311e14f058fc52b8a
Eridanus-ui <njigupaul22@gmail.com>
2024-09-28 21:32:28 +0300

    Setup and tried fonts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <div className="text-blue-800 font-imprima">App</div>
+    </>
+  );
 }
 
 export default App;
