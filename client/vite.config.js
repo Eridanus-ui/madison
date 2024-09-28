c5742d95d4f5830e9d8f1ad7b8b11130c8c0e614
Eridanus-ui <njigupaul22@gmail.com>
2024-09-29 00:28:57 +0300

    Added new packages

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // Proxy API requests to local server
+        target: "http://localhost:3000",
       },
     },
   },
